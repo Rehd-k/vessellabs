1826712fd1cacc94160bb665229ef6a154e876a9
Rehd-k <51922249+Rehd-k@users.noreply.github.com>
2026-01-02 04:23:23 +0100

    uploades

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  devIndicators: { buildActivity: false },
+  webpack: (config: { cache: boolean; plugins: any[]; }) => {
+    config.cache = false;
+    config.plugins = config.plugins?.filter((plugin: { constructor: { name: any; }; }) => {
+      const name = plugin.constructor?.name;
+      return !name?.includes('Cache');
+    });
+    return config;
+  },
+  outputFileTracingExcludes: {
+    '*': ['cache/**/*', '**/cache/**/*'],
+  },
+  experimental: {
+    webpackBuildWorker: false,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
